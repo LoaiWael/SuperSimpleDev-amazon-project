2b8947e37a52818769a4f34aef3b2437daaf3786
Loai Wael <princeloai70@gmail.com>
2025-05-03 18:49:57 +0300

    UPD orders.js

--- js/orders.js
+++ js/orders.js
@@ -1,5 +1,5 @@
 const orders = JSON.parse(localStorage.getItem('orders'));
-const cartQuantity = JSON.parse(localStorage.getItem('cartQuantity')) || 0;
+let cartQuantity = JSON.parse(localStorage.getItem('cartQuantity')) || 0;
 
 document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 
@@ -42,18 +42,16 @@ if (orders) {
             <div class="product-quantity">
               Quantity: ${product.quantity}
             </div>
-            <button class="buy-again-button button-primary">
+            <button class="buy-again-button js-buy-again-button button-primary" data-product-id="${product.id}">
               <img class="buy-again-icon" src="images/icons/buy-again.png">
-              <span class="buy-again-message">Buy it again</span>
+              <span class="buy-again-message js-buy-again-message">Buy it again</span>
             </button>
           </div>
 
           <div class="product-actions">
-            <a href="tracking.html">
-              <button class="track-package-button button-secondary">
+              <button class="track-package-button button-secondary" data-product-obj="${JSON.stringify(product).replace(/"/g, '&quot;')}">
                 Track package
               </button>
-            </a>
           </div>
       `;
     });
@@ -84,5 +82,35 @@ if (orders) {
         </div>
       </div>
       `);
+  });
+
+  const cart = JSON.parse(localStorage.getItem('cart')) || [];
+  document.querySelectorAll('.js-buy-again-button').forEach(button => {
+    button.addEventListener('click', () => {
+      let found = false;
+      for (let i = 0; i < cart.length; i++) {
+        if (cart[i].ID === button.dataset.productId) {
+          found = true;
+          cart[i].quantity++;
+          break;
+        }
+      }
+      if (!found) {
+        cart.push({
+          ID: button.dataset.productId,
+          quantity: 1
+        });
+      }
+      cartQuantity++;
+      localStorage.setItem('cart', JSON.stringify(cart));
+      localStorage.setItem('cartQuantity', cartQuantity);
+
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+      const lastInner = button.innerHTML;
+      button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-check-fill" viewBox="0 0 16 16"><path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m-1.646-7.646-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L8 8.293l2.646-2.647a.5.5 0 0 1 .708.708"/></svg> Added';
+      setTimeout(() => {
+        button.innerHTML = lastInner;
+      }, 2300);
+    });
   });
 }
