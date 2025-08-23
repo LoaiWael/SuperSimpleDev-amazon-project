54b207b9483a61afc6fdd94bc6b9d26af6b85f80
Loai Wael <princeloai70@gmail.com>
2025-08-23 16:30:24 +0300

    UPD tracking.js

--- js/tracking.js
+++ js/tracking.js
@@ -1,21 +1,33 @@
 import { cartOBJ } from '../data/cart.js';
+import { loadProducts } from '../data/products.js';
 
 document.querySelector('.js-cart-quantity').innerHTML = cartOBJ.getCartQuantity();
 
-document.querySelector('.js-delivery-date').innerHTML = ` Arriving on ${trackingProduct.shippingDate.dayName}, ${trackingProduct.shippingDate.month} ${trackingProduct.shippingDate.dayNum}`;
-document.querySelector('.js-product-info-name').innerHTML = trackingProduct.name;
-document.querySelector('.js-product-info-quantity').innerHTML = `Quantity: ${trackingProduct.quantity}`;
-document.querySelector('.js-product-image').src = trackingProduct.image;
+const currentURLObj = new URL(window.location.href);
+const trackingProduct = JSON.parse(currentURLObj.searchParams.get('product'));
+const productArrivalDate = JSON.parse(currentURLObj.searchParams.get('arrivalDate'));
 
-const today = new Date();
+loadProducts().then(products => {
+  for (let product of products) {
+    if (product.id === trackingProduct.id) {
+      document.querySelector('.js-delivery-date').innerHTML = ` Arriving on ${productArrivalDate.dayName}, ${productArrivalDate.month} ${productArrivalDate.dayNum}`;
+      document.querySelector('.js-product-info-name').innerHTML = product.name;
+      document.querySelector('.js-product-info-quantity').innerHTML = `Quantity: ${trackingProduct.quantity}`;
+      document.querySelector('.js-product-image').src = product.image;
 
-const deliverPercentage = (today.getDate() / trackingProduct.shippingDate.dayNum) * 100;
-document.querySelector('.js-progress-bar').style.width = `${deliverPercentage}%`;
+      const today = new Date();
 
-const progressLabel = document.querySelectorAll('.js-progress-label');
-progressLabel.forEach(label => {
-  label.classList.remove('current-status');
-});
+      const deliverPercentage = (today.getDate() / productArrivalDate.dayNum) * 100;
+      document.querySelector('.js-progress-bar').style.width = `${deliverPercentage}%`;
 
-let index = (deliverPercentage === 100) ? 2 : (deliverPercentage >= 50 ? 1 : 0);
-progressLabel[index].classList.add('current-status');
+      const progressLabel = document.querySelectorAll('.js-progress-label');
+      progressLabel.forEach(label => {
+        label.classList.remove('current-status');
+      });
+
+      let index = (deliverPercentage === 100) ? 2 : (deliverPercentage >= 50 ? 1 : 0);
+      progressLabel[index].classList.add('current-status');
+      break;
+    }
+  }
+})
